10574ede70b4f62a041ca562bb0be189ead5e297
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-05-23 19:14:56 +0200

    update denoify

--- deno_dist/lib/Cat.ts
+++ deno_dist/lib/Cat.ts
@@ -1,9 +1,9 @@
 
 import * as interfaces from "./interfaces/index.ts";
-import * as runExclusive from "https://raw.github.com/garronej/run_exclusive/2.1.18/mod.ts";
-import { buildMethod } from "https://raw.github.com/garronej/run_exclusive/2.1.18/lib/runExclusive.ts";
+import * as runExclusive from "https://raw.githubusercontent.com/garronej/run_exclusive/2.1.18/mod.ts";
+import { buildMethod } from "https://raw.githubusercontent.com/garronej/run_exclusive/2.1.18/lib/runExclusive.ts";
 import { load } from "https://deno.land/x/js_yaml_port@3.14.0/js-yaml.js";
-import { Md5 } from "https://raw.github.com/garronej/ts-md5/1.2.7/mod.ts";
+import { Md5 } from "https://raw.githubusercontent.com/garronej/ts-md5/1.2.7/mod.ts";
 import * as path from "https://deno.land/std/node/path.ts";
 
 export class Cat implements interfaces.Cat {
